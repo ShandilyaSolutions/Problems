048c4b45029b916b33b55e93a3d58d73f8d19067
Amritanshu Shandilya <sandyamritanshu9715@gmail.com>
2022-12-07 20:58:45 +0530

    Changes the file structure

---
+++ hackerrank/projectEuler/.ccls-cache/@home@shiv@Problems@hackerrank@projectEuler/Multiplesof3and5.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void calculateSum(int n)
+{
+    // int i=0,j=1,sum=0;
+    /*
+    sum = So+Se;   
+    cout<<sum<<endl; */
+}
+
+int main()
+{
+    int c,t,n;
+    cin>>t;
+    for(c=0;c<t;c++)
+    {
+        cin>>n;
+        calculateSum(n);
+    }
+    return 0;
+}
